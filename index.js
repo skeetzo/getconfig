40e336cb6dca02fd854b42199368e821ba512d49
Skeetzo <WebmasterSkeetzo@gmail.com>
2025-04-29 15:17:05 -0700

    ems

--- index.js
+++ index.js
@@ -244,7 +244,6 @@ internals.init = function () {
             process.env.GETCONFIG_ROOT);
 
     const root = override ? Path.resolve(process.cwd(), override) : internals.findConfig();
-    console.log(root)
     internals.primeEnv(root);
     const isDev = !process.env.hasOwnProperty('NODE_ENV');
     const devEnvirons = ['dev', 'devel', 'develop', 'development'];
